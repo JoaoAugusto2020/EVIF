45e1a3486f70cde5ff3bb0549e015274c229a0d8
João Augusto Marciano Silva <68918598+JoaoAugusto2020@users.noreply.github.com>
2025-01-05 01:55:41 -0300

    homepage
    Card, Filtro, Rodapé

---
+++ js/home.js
@@ -0,0 +1,26 @@
+$(function () {
+    document.getElementById('titleFilter').addEventListener('input', filtroCards);
+    // $("#titleFilter").keypress(filtroCards()); //Não funcionou
+    document.querySelectorAll('input[name="typeFilter"]').forEach(radio => {
+            radio.addEventListener('change', filtroCards);
+    });
+    // $("input[name='typeFilter']:checked").on("change", filtroCards()); //Não funcionou
+
+    $("#pesquisar").click(filtroCards());
+    function filtroCards() {
+            const titleFilter = document.getElementById('titleFilter').value.toLowerCase();
+            const typeFilter = document.querySelector('input[name="typeFilter"]:checked').value.toLowerCase();
+            const cards = document.querySelectorAll('.card-item');
+
+            cards.forEach(item => {
+                    const title = item.getAttribute('data-title').toLowerCase();
+                    const type = item.getAttribute('data-type').toLowerCase();
+
+                    if ((title.includes(titleFilter) || titleFilter === '') && (type === typeFilter || typeFilter === '')) {
+                            item.style.display = 'block';
+                    } else {
+                            item.style.display = 'none';
+                    }
+            });
+    }
+});
